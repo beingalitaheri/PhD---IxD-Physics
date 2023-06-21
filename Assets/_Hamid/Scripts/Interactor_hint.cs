bf5695f7b27f43df3a6b8044497eaa0c993ba61f
jamaligames <78680851+jamaligames@users.noreply.github.com>
2023-06-21 10:17:38 +0330

    Add Part of Codes

---
+++ Assets/_Hamid/Scripts/Interactor_hint.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor_hint : MonoBehaviour
+{
+    private HologramUiComponent _hintedComponent;
+    public HologramUiComponent HintedComponenet => _hintedComponent;
+
+    public void SetComponenet(HologramUiComponent _componenet)
+    {
+        bool notNullAndDifferent = false;
+
+        if (_hintedComponent && _componenet)
+        {
+            notNullAndDifferent = _hintedComponent.GetInstanceID() != _componenet.GetInstanceID();
+        }
+
+        if (!_hintedComponent || !_componenet || notNullAndDifferent)
+        {
+            if (_hintedComponent)
+            {
+                _hintedComponent.Focus(false);
+            }
+
+            if (_componenet)
+            {
+                _componenet.Focus(true);
+            }
+            _hintedComponent = _componenet;
+        }
+    }
+}
