2f987cba42dfbc5ec1adf8a7f0fe6cec69c6a22e
jamaligames <78680851+jamaligames@users.noreply.github.com>
2023-07-17 03:37:00 +0330

    Add UI_Hand

---
+++ Assets/_Main/Scripts/ReCode/OpenUI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenUI : MonoBehaviour
+{
+    [Header("( Choose Mode : )")]
+    public bool _OpenHandUI,_OpenMainMenu;
+    bool _Open;
+    //
+    public GameObject _HandMenu;
+    //
+    public void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.tag == "FingerTip") 
+        {
+            if (_Open) 
+            {
+                _Open = false;
+                if (_OpenHandUI)
+                {
+                    _HandMenu.SetActive(false);
+                }
+                else if ( _OpenMainMenu )
+                {
+                    MainUI MUI = FindObjectOfType<MainUI>();
+                    MUI.CloseUI();
+                }
+                 GetComponent<ButtonClick>()._ButtonClicked();
+            }
+            else 
+            {
+                _Open = true;
+                if (_OpenHandUI)
+                {
+                    _HandMenu.SetActive(true);
+                }
+                else if (_OpenMainMenu)
+                {
+                    MainUI MUI = FindObjectOfType<MainUI>();
+                    MUI.OpenUI();
+                }
+                GetComponent<ButtonClick>()._ButtonClicked();
+
+            }
+        }
+    }
+    //
+}
